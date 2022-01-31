ae7cd7d7e6b7664beafdf881fdc3845c05f5d7fe
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-01-31 20:59:22 +0000

    Created back-end Express server for project

--- server/server.js
+++ server/server.js
@@ -8,11 +8,6 @@ App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
 
-// Sample GET route
-App.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
-
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
