88cf18f52db282e1bb1a5ddb2c658f03492d6f1d
Julie Fitzpatrick <juliefitzpatrick@Julies-MacBook-Air.local>
2022-02-01 11:03:31 -0330

    creates ratings table schema and inserts category names

---
+++ server/src/db/schema/development.sql
@@ -0,0 +1,10 @@
+INSERT INTO categories (category)
+VALUES
+    ('Home Improvement'),
+    ('Yard and Lawn'),
+    ('Miscellaneous'),
+    ('House Work'),
+    ('Pet Care'),
+    ('Personal Care'),
+    ('Technical Work'),
+    ('Errands');
