a5fd19bcf258c1baa90c30b00582817b7baa41b0
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-02-01 11:34:08 -0330

    creates several listing seed data. reorganizes seed data

---
+++ server/src/db/seeds/listings.sql
@@ -0,0 +1,23 @@
+INSERT INTO listings (creator_id, title, description, image_1, price, city, province, postal_code, country)
+VALUES
+    (1, 'Mow My Lawn', 'Looking for someone to mow my lawn this week', 'https://images.unsplash.com/photo-1594498653385-d5172c532c00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80', 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    (2, 'Paint Home Exterior', 'Seeking someone to paint the wooden siding of my house. Scaffolding and supplies provided', 'https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80', 1000, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    (1, 'Pick Up Groceries', 'Looking for a reliable person to collect my grocery order and deliver it to my home', 'https://media.istockphoto.com/photos/shopping-bag-full-of-fresh-vegetables-and-fruits-with-milk-and-eggs-picture-id1283628398?b=1&k=20&m=1283628398&s=170667a&w=0&h=pKLURb1pktkk5Qg8w_huxla5xG1OMEEyKaDA3TDBfd8=', 50, 'Toronto', 'Ontario', 'A5T3BF', 'Canada');
+    -- (3, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (4, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (2, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (6, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (2, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (7, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (4, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (3, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (14, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (10, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (11, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (9, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (8, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (6, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (5, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (12, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (13, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada'),
+    -- (7, '', , 100, 'Toronto', 'Ontario', 'A5T3BF', 'Canada');
