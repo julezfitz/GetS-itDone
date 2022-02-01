745a92bc48f94d28a8d9e90669372d79911a83a1
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-02-01 12:59:46 -0330

    fixes schema and seed data to work. Everything in working order now

---
+++ server/src/db/seeds/01-categories.sql
@@ -0,0 +1,11 @@
+INSERT INTO categories (id, category)
+VALUES
+    (1, 'Home Improvement'),
+    (2, 'Painting'),
+    (3, 'Yard and Lawn'),
+    (4, 'Miscellaneous'),
+    (5, 'House Work'),
+    (6, 'Pet Care'),
+    (7, 'Personal Care'),
+    (8, 'Technical Work'),
+    (9, 'Errands');
