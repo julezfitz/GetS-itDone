c7bec59e58be96e2407683167073d59f7d81e437
Matt Parisien <matthewparisien4@gmail.com>
2022-02-01 15:37:43 -0500

    Add temporary pages for react-router-dom demo

---
+++ client/src/pages/Create.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Create() {
+  return <div>
+    <h1>Hello this is the create view</h1>
+  </div>;
+}
+
+export default Create;
---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Dashboard() {
+  return <div>
+    <h1>Hello this is the dashboard page</h1>
+  </div>;
+}
+
+export default Dashboard;
