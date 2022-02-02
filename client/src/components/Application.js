e9c803837871d7e335a2b5f1619f5cef740807fa
Matt Parisien <matthewparisien4@gmail.com>
2022-02-02 08:40:27 -0500

    Add basic react routing with demo links for later discussion

--- client/src/components/Application.js
+++ client/src/components/Application.js
@@ -1,29 +1,39 @@
 import React from "react";
 import "./Application.scss";
 import { Routes, Route, Link } from "react-router-dom";
+import Create from "./pages/Create";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Update from "./pages/Update";
 
 export default function Application() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Routes>
-        <Route path="/"/>
-        <Route path="/create"/>
-        <Route path="/update/:id/"/>
-        <Route path="/update/:id/"/>
-      </Routes>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<p>
+					Edit <code>src/App.js</code> and save reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</header>
+      <div className="demo-links">
+        <Link to={"/"}>Dashboard view</Link>
+        <Link to="/create">Create view</Link>
+        <Link to="/profile">Profile view</Link>
+        <Link to="/update/:id">Update view</Link>
+      </div>
+			<Routes>
+				<Route path='/' element={<Dashboard />} />
+				<Route path='/create' element={<Create />} />
+				<Route path='/profile' element={<Profile />} />
+				<Route path='/update/:id/' element={<Update />} />
+			</Routes>
+		</div>
+	);
 }
