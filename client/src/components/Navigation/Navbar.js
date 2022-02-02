b4208616178ce99b22621e268d1174224affb8d8
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-02-02 12:22:03 -0800

    Added search bar into navigation bar

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -2,15 +2,18 @@ import React from "react";
 import "./Navigation.scss";
 import NavItem from "./NavItem";
 import DropdownMenu from "./DropdownMenu";
-// import SearchBar from "../../helpers/SearchBar";
+import Searchbar from "./Searchbar";
 
 export default function Navbar(props) {
   return (
     <nav className="navbar">
-      {/* <p>Get S*it Done</p> */}
+      <ul className="site-name">
+        <p>Get S*it Done</p>
+      </ul>
+      <ul className="search">
+          <Searchbar/>
+        </ul>
       <ul className="navbar-nav">
-        <NavItem icon="L"/>
-        <NavItem icon="S"/>
         <NavItem icon="P">
           <DropdownMenu />
         </NavItem>
