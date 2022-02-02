2b0d24f3f72033d4a230b078d1c8d2650d1c325e
Matt Parisien <matthewparisien4@gmail.com>
2022-02-02 13:15:54 -0500

    Remove user_notifications seeds for now

--- server/src/db/seeds/06-notifications.sql
+++ server/src/db/seeds/06-notifications.sql
@@ -2,4 +2,4 @@ INSERT INTO notifications (message)
 VALUES
  ('Your offer has been declined.'),
  ('Your offer has been accepted!'),
- ('You have a new offer!')
+ ('You have a new offer!');
--- server/src/db/seeds/07-user_notifications.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO user_notifications (user_id, notification_id)
-VALUES
- ("Your offer has been declined."),
- ("Your offer has been accepted!"),
- ("You have a new offer!")
