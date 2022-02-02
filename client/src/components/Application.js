a9a7037288b0ce56a648f53edc2fc338f643ba0e
Matt Parisien <matthewparisien4@gmail.com>
2022-02-02 08:51:02 -0500

    Complete react router demo

--- client/src/components/Application.js
+++ client/src/components/Application.js
@@ -5,6 +5,7 @@ import Create from "./pages/Create";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
 import Update from "./pages/Update";
+import Search from "./pages/Search";
 
 export default function Application() {
 	return (
@@ -22,17 +23,21 @@ export default function Application() {
 					Learn React
 				</a>
 			</header>
-      <div className="demo-links">
-        <Link to={"/"}>Dashboard view</Link>
-        <Link to="/create">Create view</Link>
-        <Link to="/profile">Profile view</Link>
-        <Link to="/update/:id">Update view</Link>
-      </div>
+			<p style={{ margin: "0 auto" }}>
+				This is just to demo the router for our discussion later
+			</p>
+			<div className='demo-links'>
+				<Link to={"/"}>Dashboard view</Link>
+				<Link to='/create'>Create view</Link>
+				<Link to='/profile'>Profile view</Link>
+				<Link to='/update/:id'>Update view</Link>
+			</div>
 			<Routes>
 				<Route path='/' element={<Dashboard />} />
 				<Route path='/create' element={<Create />} />
 				<Route path='/profile' element={<Profile />} />
-				<Route path='/update/:id/' element={<Update />} />
+				<Route path='/search' element={<Search />} />
+				<Route path='/update/:id' element={<Update />} />
 			</Routes>
 		</div>
 	);
