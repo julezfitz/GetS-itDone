f150fc8b86646fcfaa8018e3fa776730f48de182
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-02-02 12:09:50 -0330

    adds openapi draft routes documentation

--- server/src/routes/listings.js
+++ server/src/routes/listings.js
@@ -2,16 +2,202 @@ const router = require("express").Router();
 
 module.exports = db => {
 
-  router.get("/listings", (request, response) => {
-    db.query(`SELECT * FROM listings`).then(({ rows: listings }) => {
-      response.json(
-        listings.reduce(
-          (previous, current) => ({ ...previous, [current.id]: current }),
-          {}
-        )
-      );
+    router.get("/listings", (request, response) => {
+        db.query(`SELECT * FROM listings`).then(({ rows: listings }) => {
+            response.json(
+                listings.reduce(
+                    (previous, current) => ({ ...previous, [current.id]: current }),
+                    {}
+                )
+            );
+        });
     });
-  });
 
-  return router;
+    return router;
 };
+
+// API documentation for Open API below
+
+module.exports.apiDocs = {
+    "/listings": {
+        "get": {
+            "description": "Return all listings from the system. Narrows results by query string if one is provided.",
+            "parameters": {
+                "keywordsParam": {
+                    "name": "keywords", 
+                    "in": "query",
+                    "description": "search listings by keywords",
+                    "required": false
+                },
+                "categoryParam": {
+                    "name": "category",
+                    "in": "query",
+                    "description": "filter listings by category",
+                    "required": false
+                }
+            },
+            "responses": {
+                "200": {
+                    "description": "An individual listing.",
+                    "content": {
+                        "application/json": {
+                            "schema": {},
+                            "examples": [
+                                {
+                                    "id": 1,
+                                    "creatorId": 2,
+                                    "title": "Mow My Lawn",
+                                    "description": "Looking for someone to mow my lawn this week",
+                                    "created": "2022-01-01 05:01:37 -5:00",
+                                    "image_1": "https://images.unsplash.com/image_1.jpg",
+                                    "price": 200.99,
+                                },
+                                {
+                                    "id": 2,
+                                    "creatorId": 1,
+                                    "title": "Cut Grass",
+                                    "description": "Looking for someone to cut grass",
+                                    "created": "2022-01-12 05:01:37 -5:00",
+                                    "image_1": "https://images.unsplash.com/image_1.jpg",
+                                    "price": 200.99,
+                                }
+                                
+                            ]
+                        }
+                    }
+                }
+            }
+        },
+        "post": {
+            "description": "Create a listing",
+            "requestBodies": {
+                "description": "listing model",
+                "content": {
+                    "application/json": {
+                        "schema": {},
+                        "examples": {
+                            "individualListing": {
+                                "id": 1,
+                                "creatorId": 2,
+                                "title": "Mow My Lawn",
+                                "description": "Looking for someone to mow my lawn this week",
+                                "created": "2022-01-01 05:01:37 -5:00",
+                                "image_1": "https://images.unsplash.com/image_1.jpg",
+                                "image_2": "https://images.unsplash.com/image_2.png",
+                                "image_3": "https://images.unsplash.com/image_3.svg",
+                                "price": 200.99,
+                                "city": "Toronto",
+                                "province": "Ontario",
+                                "postalCode": "A5T3BF",
+                                "country": "Canada"
+                            }
+                        }
+                    }
+                }
+            },
+            "responses": {
+                "201": {
+                    "description": "Listing Created",
+                },
+            }
+        }
+    },
+    "/listings/:listingId": {
+        "parameters": [
+            {
+                "name": "listingId",
+                "in": "path",
+                "required": true,
+                "description": "listing ID of the individual listing"
+            }
+        ],
+        "get": {
+            "description": "Returns the details of an individual listing.",
+            "responses": {
+                "200": {
+                    "description": "An individual listing.",
+                    "content": {
+                        "application/json": {
+                            "schema": {},
+                            "examples": {
+                                "individualListing": {
+                                    "id": 1,
+                                    "creator": {
+                                        "id": 2,
+                                        "firstName": "Johnny",
+                                        "lastName": "Smith"
+                                    },
+                                    "title": "Mow My Lawn",
+                                    "description": "Looking for someone to mow my lawn this week",
+                                    "created": "2022-01-01 05:01:37 -5:00",
+                                    "image_1": "https://images.unsplash.com/image_1.jpg",
+                                    "image_2": "https://images.unsplash.com/image_2.png",
+                                    "image_3": "https://images.unsplash.com/image_3.svg",
+                                    "price": 100.99,
+                                    "city": "Toronto",
+                                    "province": "Ontario",
+                                    "postalCode": "A5T3BF",
+                                    "country": "Canada"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        "put": {
+            "description": "Update a listing",
+            "requestBodies": {
+                "description": "listing model",
+                "content": {
+                    "application/json": {
+                        "schema": {},
+                        "examples": {
+                            "individualListing": {
+                                "id": 1,
+                                "creatorId": 2,
+                                "title": "Mow My Lawn",
+                                "description": "Looking for someone to mow my lawn this week",
+                                "created": "2022-01-01 05:01:37 -5:00",
+                                "image_1": "https://images.unsplash.com/image_1.jpg",
+                                "image_2": "https://images.unsplash.com/image_2.png",
+                                "image_3": "https://images.unsplash.com/image_3.svg",
+                                "price": 100.99,
+                                "city": "Toronto",
+                                "province": "Ontario",
+                                "postalCode": "A5T3BF",
+                                "country": "Canada"
+                            }
+                        }
+                    }
+                }
+            },
+            "responses": {
+                "200": {
+                    "description": "Updated ok",
+                    "content": {
+                        "application/json": {
+                            "schema": {},
+                            "examples": {
+                                "id": 1,
+                                "creatorId": 2,
+                                "title": "Mow My Lawn",
+                                "description": "Looking for someone to mow my lawn this week",
+                                "created": "2022-01-01 05:01:37 -5:00",
+                                "image_1": "https://images.unsplash.com/image_1.jpg",
+                                "image_2": "https://images.unsplash.com/image_2.png",
+                                "image_3": "https://images.unsplash.com/image_3.svg",
+                                "price": 100.99,
+                                "city": "Toronto",
+                                "province": "Ontario",
+                                "postalCode": "A5T3BF",
+                                "country": "Canada"
+                            }
+                        }
+                    },
+                },
+            },
+
+        }
+    }
+}
---
+++ server/src/routes/users.js
@@ -0,0 +1,147 @@
+
+// API documentation for Open API below
+
+module.exports.apiDocs = {
+    "/user/register": {
+        "post": {
+            "description": "Create a user account",
+            "requestBodies": {
+                "description": "user model",
+                "content": {
+                    "application/json": {
+                        "schema": {},
+                        "examples": {
+                            "individualUser": {
+                                "firstName": "Johnny", 
+                                "lastName": "Smith", 
+                                "email": "jsmith@email.com", 
+                                "password": "password", 
+                                "city": "Toronto", 
+                                "province": "Ontario", 
+                                "postalCode": "A5T3BF", 
+                                "country": "Canada", 
+                                "image": "https://images.unsplash.com/profile.svg"
+                            }
+                        }    
+                    }
+                }
+            },
+            "responses": {
+                "201": {
+                    "description": "User Created",
+                },
+            }
+        }
+    },
+    "/user/session": {
+        "post": {
+            "description": "Login to a user account",
+            "requestBodies": {
+                "description": "user email and password",
+                "content": {
+                    "application/json": {
+                        "schema": {},
+                        "examples": {
+                            "individualUserCredentials": {
+                                "email": "jsmith@email.com", 
+                                "password": "password", 
+                            }
+                        }    
+                    }
+                }
+            },
+            "responses": {
+                "201": {
+                    "description": "Session Created",
+                    "content": {
+                        "application/json": {
+                            "schema": {},
+                            "examples": {
+                                "individualUser": {
+                                    "firstName": "Johnny", 
+                                    "lastName": "Smith", 
+                                    "email": "jsmith@email.com", 
+                                    "password": "password", 
+                                    "city": "Toronto", 
+                                    "province": "Ontario", 
+                                    "postalCode": "A5T3BF", 
+                                    "country": "Canada", 
+                                    "image": "https://images.unsplash.com/profile.svg"
+                                },
+
+                            }
+                         }
+                    }
+                }
+            }
+        },
+        "delete": {
+            "description": "Logout of a user account",
+            "responses": {
+                204: {
+                  description: "Session terminated",
+                },
+            },
+        }
+    },
+
+    "/user/:userId": {
+        "put": {
+            "description": "Update a user account",
+            "parameters": {
+                "pathParam": {
+                    "name": "userId", 
+                    "in": "path",
+                    "description": "update user by id",
+                    "required": true
+                }
+            },
+            "requestBodies": {
+                "description": "user model",
+                "content": {
+                    "application/json": {
+                        "schema": {},
+                        "examples": {
+                            "individualUser": {
+                                "firstName": "John", 
+                                "lastName": "Smith", 
+                                "email": "jsmi@email.com", 
+                                "password": "password1", 
+                                "city": "Toronto", 
+                                "province": "Ontario", 
+                                "postalCode": "B5T3BF", 
+                                "country": "Canada", 
+                                "image": "https://images.unsplash.com/profile.svg"
+                            }
+                        }    
+                    }
+                }
+            },
+            "responses": {
+                "200": {
+                    "description": "User updated",
+                    "content": {
+                        "application/json": {
+                            "schema": {},
+                            "examples": {
+                                "individualUser": {
+                                    "firstName": "Johnny", 
+                                    "lastName": "Smith", 
+                                    "email": "jsmith@email.com", 
+                                    "password": "password", 
+                                    "city": "Toronto", 
+                                    "province": "Ontario", 
+                                    "postalCode": "A5T3BF", 
+                                    "country": "Canada", 
+                                    "image": "https://images.unsplash.com/profile.svg"
+                                },
+
+                            }
+                         }
+                    }
+                },
+            }
+        }
+    },
+
+}
