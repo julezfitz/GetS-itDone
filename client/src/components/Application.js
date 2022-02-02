56a60cffd8aa9d46befe5b80e584b5d95b67b35a
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-02-01 16:21:45 -0800

    Created components folder and initial Application component

--- client/src/components/Application.js
+++ client/src/components/Application.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Application.scss";
 
-function App() {
+export default function Application() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save reload.
         </p>
         <a
           className="App-link"
@@ -21,5 +20,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Application from './components/Application';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Application />
   </React.StrictMode>,
   document.getElementById('root')
 );
