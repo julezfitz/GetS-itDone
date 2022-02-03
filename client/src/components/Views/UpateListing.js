b2064daa3f88ab19c1d2221d657f59b405beecbb
Matt Parisien <matthewparisien4@gmail.com>
2022-02-03 08:21:35 -0500

    Minor changes

--- client/src/components/Views/UpateListing.js
+++ client/src/components/Views/UpateListing.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function UpateListing() {
-  return <div>
-    <h1>This is the update a user's Listings view</h1>
-  </div>;
+	return (
+		<div className='updateListing-wrapper'>
+			<h1>This is the update a user's Listings view</h1>
+		</div>
+	);
 }
 
 export default UpateListing;
