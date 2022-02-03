4604cf04ec564db9bdeff4b77f40130ce09533a2
Matt Parisien <matthewparisien4@gmail.com>
2022-02-03 10:56:11 -0500

    Begin implementing passport local strategy

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,1 +1,18 @@
 const LocalStrategy = require("passport");
+const bcrypt = require("bcrypt");
+
+module.exports = (db, passport) => {
+	passport.use(
+		new LocalStrategy((username, password, done) => {
+			db.query(
+				`
+        SELECT id, username, password
+        FROM users
+        WHERE username = $1
+        AND password = #2
+      `,
+				[username, password]
+			);
+		})
+	);
+};
