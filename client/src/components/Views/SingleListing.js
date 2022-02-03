240662286ba02f7a4271a4caeac0ac3f48783bdb
Matt Parisien <matthewparisien4@gmail.com>
2022-02-03 08:00:08 -0500

    Add single listing component

---
+++ client/src/components/Views/SingleListing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SingleListing() {
+	return (
+		<div>
+			<h1>This will be the view to display a single listing</h1>
+		</div>
+	);
+}
+
+export default SingleListing;
