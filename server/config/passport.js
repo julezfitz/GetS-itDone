cf977e25d66034f798562416ded10e8eb8176d65
Matt Parisien <matthewparisien4@gmail.com>
2022-02-03 11:35:57 -0500

    Implement basic post routing for login view

--- server/config/passport.js
+++ server/config/passport.js
@@ -4,15 +4,28 @@ const bcrypt = require("bcrypt");
 module.exports = (db, passport) => {
 	passport.use(
 		new LocalStrategy((username, password, done) => {
+			//Match user
 			db.query(
 				`
         SELECT id, username, password
         FROM users
         WHERE username = $1
-        AND password = #2
+        AND password = $2
       `,
 				[username, password]
-			);
+			).then(res => {
+				if (res.rows.length > 0) {
+					const user = res.rows[0];
+					bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+						if (isMatch) {
+							return done(null, { id: user.id, email: user.email });
+						} else {
+              return done(null, false, { message: "Incorrect password" })
+            }
+					});
+				}
+			});
 		})
 	);
 };
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 
 const Express = require('express');
 const Passport = require("passport");
+const cors = require("cors");
 const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
@@ -8,6 +9,7 @@ const PORT = 8080;
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
+App.use(cors());
 App.use(Express.static('public'));
 App.use(Passport.initialize());
 App.use(Passport.session());
