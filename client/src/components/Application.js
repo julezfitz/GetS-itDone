deae50bb5a5856818fb5802dda889cd3d1271b50
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-02-02 16:03:00 -0800

    Created the register modal. Still needs more work

--- client/src/components/Application.js
+++ client/src/components/Application.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import "./Application.scss";
 import Navbar from './Navigation/Navbar';
-
+import Register from "./Register/Register";
 
 export default function Application() {
+  const [showRegister, setShowRegister] = useState(false)
+
+  const openRegister = () => {
+    setShowRegister(prev => !prev);
+  }
+
   return (
-    <Navbar/>
+    <div className="app">
+      <Navbar/>
+      <div className="container">
+        <button className="button" onClick={openRegister}>Register Modal</button>
+        <Register showRegister={showRegister} setShowRegister={setShowRegister} />
+      </div>
+    </div>
   );
 }
