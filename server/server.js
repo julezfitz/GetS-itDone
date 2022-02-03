d7c4c87495819cdfd705c26530119c25c1f9372e
Matt Parisien <matthewparisien4@gmail.com>
2022-02-03 09:49:51 -0500

    Begin passport

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
+
 const Express = require('express');
+const Passport = require("passport");
 const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
@@ -7,6 +9,8 @@ const PORT = 8080;
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
+App.use(Passport.initialize());
+App.use(Passport.session());
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
