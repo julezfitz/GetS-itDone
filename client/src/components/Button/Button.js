2b56f17e7fe500171b7cfa6f4bd09440d0fab3a8
Matt Parisien <matthewparisien4@gmail.com>
2022-02-04 17:51:50 -0500

    Final push for the day

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -2,7 +2,8 @@ import React from "react";
 import { StyledButton } from "./styles";
 
 function Button({ children, type, color }) {
-	/* Style can either be solid or outline */
+	/* Type can either be solid or outline */
+	/* Color can either be "light" or "dark" or "gradient" */
 
 	return (
 		<StyledButton className='button' type={type} color={color}>
