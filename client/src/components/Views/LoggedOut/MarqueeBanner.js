cc0f3fa72f6655ef8494b0bb78f1e3bd846ecedf
Matt Parisien <matthewparisien4@gmail.com>
2022-02-04 14:29:26 -0500

    Begin implementing home hero section

---
+++ client/src/components/Views/LoggedOut/MarqueeBanner.js
@@ -0,0 +1,11 @@
+import Marquee from "react-fast-marquee";
+
+function MarqueeBanner() {
+  return (
+    <Marquee>
+      This is some marquee text
+    </Marquee>
+  );
+}
+
+export default MarqueeBanner;
---
+++ client/src/components/Views/styles/styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const StyledHero = styled.section`
+  height: 50vw;
+`
