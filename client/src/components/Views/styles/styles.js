415d0ec512ca12803dccf7e8e67aa8e1c7132f0a
Matt Parisien <matthewparisien4@gmail.com>
2022-02-04 16:05:18 -0500

    Implement responsive line width

--- client/src/components/Views/styles/styles.js
+++ client/src/components/Views/styles/styles.js
@@ -1,54 +1,65 @@
 import styled from "styled-components";
+import { device } from "../../../styles/devices/devices";
 
 export const StyledHero = styled.section`
 	background-color: black;
 	color: white;
-  height: 50vw;
+	height: 50vw;
 	max-height: 100vh;
 	font-family: "Helvetica";
 	font-size: 10vw;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  overflow: hidden;
-  position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	overflow: hidden;
+	position: relative;
+
+  .line {
+    width: 80%;
+  }
+
+	@media ${device.laptopL} {
+		font-size: 10rem;
+		.line {
+			width: 1200px;
+		}
+	}
 `;
 
 export const StyledMarquee = styled.div`
 	padding: 3rem 0;
-  overflow: hidden;
+	overflow: hidden;
 
-  .marquee-wrapper__inner {
-    transform: translateY(100%);
-    opacity: 0;
-    
-  }
+	.marquee-wrapper__inner {
+		transform: translateY(100%);
+		opacity: 0;
+	}
 `;
 
 export const StyledLoggedOutHome = styled.div`
-  height: 100%;
-  background-color: black;
-`
+	height: 100%;
+	background-color: black;
+`;
 
 export const StyledLeak = styled.div`
-  height: 400px;
-  width: 400px;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: orange;
-  z-index: 9999;
-  border-radius: 50%;
-  filter: blur(30px)
-`
+	height: 400px;
+	width: 400px;
+	position: absolute;
+	top: 0;
+	left: 0;
+	background-color: orange;
+	z-index: 9999;
+	border-radius: 50%;
+	filter: blur(30px);
+`;
 
 export const StyledHeroImage = styled.div`
-  width: 50vw;
-  position: absolute;
+	width: 50vw;
+	position: absolute;
 
-  img {
-    height: 100%;
-    width: 100%;
-  }
-`
+	img {
+		height: 100%;
+		width: 100%;
+	}
+`;
