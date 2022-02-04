fa7236ebb6d6e0ea8476efda699a2b0faa34f6cb
Matt Parisien <matthewparisien4@gmail.com>
2022-02-04 17:27:19 -0500

    Begin implementing curve

--- client/src/components/Views/LoggedOut/HeroImages.js
+++ client/src/components/Views/LoggedOut/HeroImages.js
@@ -1,12 +1,13 @@
 import React from "react";
 import HeroImage from "./HeroImage";
 import { items } from "../../../assets/images/characters/index";
+import Marquee from "react-fast-marquee";
 
 function HeroImages() {
 	return (
 		<>
-			{items.map(item => {
-				return <HeroImage src={item} />;
+			{items.map((item, i) => {
+				return <HeroImage src={item} key={i} />;
 			})}
 		</>
 	);
