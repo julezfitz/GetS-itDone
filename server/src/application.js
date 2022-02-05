1c2680253b58a04eb1531ed4c326786978e6ff90
Matt Parisien <matthewparisien4@gmail.com>
2022-02-05 08:04:53 -0500

    Configure cors for express session

--- server/src/application.js
+++ server/src/application.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cors = require("cors");
 const express = require("express");
 const bodyparser = require("body-parser");
+const session = require("express-session");
 const swaggerUi = require("swagger-ui-express");
 const passport = require("passport");
 const app = express();
@@ -12,7 +13,15 @@ const db = require("./db");
 module.exports = function application(ENV) {
 	app.use(bodyparser.json());
 
-	app.use(cors());
+	//Configure cors to allow for front end to access cookies
+	app.use(
+		cors({
+			origin: ["http://localhost:3000", "http://localhost:3001"],
+			methods: ["GET", "POST", "PUT", "DELETE"],
+			credentials: true,
+		})
+	);
+
 	app.use(
 		"/api-docs",
 		swaggerUi.serve,
@@ -22,8 +31,8 @@ module.exports = function application(ENV) {
 	app.use("/", require("./routes/listings")(db));
 	app.use("/", require("./routes/categories")(db));
 	app.use("/", require("./routes/users")(db));
-  app.use("/", require("./routes/ratings")(db))
-  app.use("/", require("./routes/offers")(db))
+	app.use("/", require("./routes/ratings")(db));
+	app.use("/", require("./routes/offers")(db));
 
 	//Passport config
 	require("./config/passport")(passport, db);
