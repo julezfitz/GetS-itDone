4339c80be7e46b1eb7ed768bb923d99b853d978d
Matt Parisien <matthewparisien4@gmail.com>
2022-02-06 12:48:18 -0500

    Adjust stacking

--- client/src/components/Heading/styles/styles.js
+++ client/src/components/Heading/styles/styles.js
@@ -2,13 +2,18 @@ import styled from "styled-components";
 import Kobe from "../../../assets/fonts/Kobe.woff";
 import Beatrice from "../../../assets/fonts/Beatrice.otf";
 import Opposit from "../../../assets/fonts/opposit.otf";
+import Cardinal from "../../../assets/fonts/cardinal.otf";
 
 export const StyledHeading = styled.div`
-
-
 	@font-face {
 		font-family: "Opposit";
 		src: url(${Opposit});
+		font-weight: lighter;
+	}
+
+	@font-face {
+		font-family: "Cardinal";
+		src: url(${Cardinal});
 		font-weight: lighter;
 	}
 
@@ -26,13 +31,23 @@ export const StyledHeading = styled.div`
 		font-size: 10.5rem;
 	}
 
-
-	h1, h2, h3, h4, h5, h6 {
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6 {
 		font-weight: lighter;
+
+		.font-accent {
+			font-family: 'Cardinal';
+		}
 	}
 
 	h2 {
-		font-size: 7rem;
+		font-size: 6rem;
+		letter-spacing: -0.2rem;
 		line-height: 6rem;
+		
 	}
 `;
