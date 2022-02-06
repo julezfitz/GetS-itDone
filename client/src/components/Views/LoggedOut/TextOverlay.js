94e218a6732fcc5c4ae2e21743939c276574ca94
Matt Parisien <matthewparisien4@gmail.com>
2022-02-06 13:21:40 -0500

    Use splitText to split by line and wrap each line in additional line wrapper for overflow effect

--- client/src/components/Views/LoggedOut/TextOverlay.js
+++ client/src/components/Views/LoggedOut/TextOverlay.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import MarqueeBanner from "./MarqueeBanner";
 import Heading from "../../Heading/Heading";
 import { Arrow, CircleText } from "./Vector/vector";
 import Button from "../../Button/Button";
+import SplitText from "gsap/SplitText";
+import gsap from "gsap";
+import $ from "jquery";
 
 function TextOverlay() {
+	const headingRef = useRef(null);
+	const splitRef = useRef(null);
+
+	useEffect(() => {
+		if (headingRef.current && !splitRef.current) {
+			splitRef.current = new SplitText(headingRef.current, {
+				type: "lines",
+				linesClass: "line",
+			});
+
+			console.log(splitRef.current.lines)
+			$(splitRef.current.lines).forEach(line => {
+				$(line).wrap("<div class='line-wrapper'></div>");
+			})
+		}
+	}, [headingRef]);
+
 	return (
 		<div className='text-content-wrapper'>
 			<div className='text-content-inner'>
-				<Heading size='medium' color='light'>
-					We are a people-powered ecosystem. By you, for you.
+				<Heading size='medium' color='light' ref={headingRef}>
+					A people-powered ecosystem. By you, for you.
 				</Heading>
 
 				<Button type='outline' color='light' className='hero-cta'>
