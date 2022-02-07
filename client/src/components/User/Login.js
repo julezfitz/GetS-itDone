c1d877bab0c799eb87337fe8a9154673cf3928dd
Matt Parisien <matthewparisien4@gmail.com>
2022-02-07 09:44:50 -0500

    Fix error in post register rout

--- client/src/components/User/Login.js
+++ client/src/components/User/Login.js
@@ -1,26 +1,29 @@
 import React, { useRef } from "react";
 import "./Login.scss";
-import { MdClose } from 'react-icons/md';
+import { MdClose } from "react-icons/md";
 
-export default function Login({showLogin, setShowLogin}) {
-  const loginRef = useRef();
+export default function Login({ showLogin, setShowLogin }) {
+	const loginRef = useRef();
 
-  const closeLogin = (event) => {
-    if(loginRef.current === event.target) {
-      setShowLogin(false);
-    }
-  };
+	const closeLogin = event => {
+		if (loginRef.current === event.target) {
+			setShowLogin(false);
+		}
+	};
 
-  return (
-    <>
-    {showLogin ? (
-        <div className="background" ref={loginRef} onClick={closeLogin}>
-          <div className="loginWrapper" showRegister={showLogin}>
-            <MdClose className="closeLoginButton" aria-label="Close login" onClick={() => setShowLogin(prev => !prev)}/>
-          </div>
-        </div>
-      ) : null
-    }
-    </>
-  );
+	return (
+		<>
+			{showLogin ? (
+				<div className='background' ref={loginRef} onClick={closeLogin}>
+					<div className='loginWrapper' showRegister={showLogin}>
+						<MdClose
+							className='closeLoginButton'
+							aria-label='Close login'
+							onClick={() => setShowLogin(prev => !prev)}
+						/>
+					</div>
+				</div>
+			) : null}
+		</>
+	);
 }
