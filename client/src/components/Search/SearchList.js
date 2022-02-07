78750ca61c9009015120c9de8a58af0ce44a3fe1
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-02-07 09:33:43 -0800

    Implemented Material UI into the project. Made all necessary file changes and additions

--- client/src/components/Search/SearchList.js
+++ client/src/components/Search/SearchList.js
@@ -1,12 +1,30 @@
-import React from "react";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 import SearchListItem from "./SearchListItem";
+import ListingDetails from '../Listings/ListingDetails';
 
-export default function SearchList(props) {
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  color: theme.palette.text.secondary,
+}));
+
+export default function SearchList() {
   return (
-    <ul>
-      <SearchListItem />
-    </ul>
+          <Item>
+            <ListingDetails>
+              <SearchListItem />
+            </ListingDetails>
+            <ListingDetails>
+              <SearchListItem />
+            </ListingDetails>
+            <ListingDetails>
+              <SearchListItem />
+            </ListingDetails>
+            <ListingDetails>
+              <SearchListItem />
+            </ListingDetails>
+          </Item>
   );
 }
-
-// props.days.map((day)
