087b3d5349a5db7cfb1d1c5c22121e1ae614a17c
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-02-07 14:10:25 -0800

    Testing the avatar feature in the Navbar

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -243,7 +243,7 @@ export default function ResponsiveAppBar() {
           <Box sx={{ maxWidth: "150px" }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Bob Smith" src="image1.jpg"/>
               </IconButton>
             </Tooltip>
             <Menu
