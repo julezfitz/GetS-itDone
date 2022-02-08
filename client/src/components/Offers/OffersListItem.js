a65f61da14652cef635da98e7958c9bf288ad9fd
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-02-08 08:47:26 -0800

    Added stars for rating in offers section

--- client/src/components/Offers/OffersListItem.js
+++ client/src/components/Offers/OffersListItem.js
@@ -1,37 +1,37 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
 import ButtonBase from "@mui/material/ButtonBase";
 
 export default function OffersListItem() {
+  
   return (
-      <Grid container spacing={2}>
-        <Grid item>
-        </Grid>
-        <Grid item xs={12} sm container>
-          <Grid item xs container direction="column" spacing={2}>
-            <Grid item xs>
-              <Typography gutterBottom variant="subtitle1" component="div">
-                Clean My Yard
-              </Typography>
-              <Typography variant="body2" gutterBottom>
-                I know it'll be challenging but I'd like someone to come and
-                clean up my beach. I will also need you to rake the beach so
-                that it is even.
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography variant="body2" color="text.secondary">
-                Date: February 4, 2022
-              </Typography>
-            </Grid>
+    <Grid container spacing={2}>
+      <Grid item></Grid>
+      <Grid item xs={12} sm container spacing={4}>
+        <Grid item xs container direction="column">
+          <Grid item xs>
+            <Typography gutterBottom variant="subtitle1" component="div">
+              John Smith
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              <Rating name="user-rating" size="small" value={4} readOnly />
+            </Typography>
           </Grid>
           <Grid item>
-            <Typography variant="subtitle1" component="div">
-              $500.00
+            <Typography variant="body2" color="text.secondary">
+              Date: February 4, 2022
             </Typography>
           </Grid>
         </Grid>
+        <Grid item>
+          <Typography variant="subtitle1" component="div">
+            $500.00
+          </Typography>
+        </Grid>
       </Grid>
+    </Grid>
   );
 }
