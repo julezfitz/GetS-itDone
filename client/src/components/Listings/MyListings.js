abd08ed8d5173bfdbd6828092db9ebe113ce22fe
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-02-08 16:24:40 -0330

    adds functionality for mylistings views and components. Handles API calls to populate each

--- client/src/components/Listings/MyListings.js
+++ client/src/components/Listings/MyListings.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
@@ -6,6 +6,8 @@ import Grid from "@mui/material/Grid";
 import MyListingItem from "./MyListingItem";
 import ListingQuickView from './ListingQuickView';
 import OffersList from "../Offers/OffersList";
+import axios from "axios";
+import { UserContext } from "../Application.js";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -14,21 +16,37 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function MyListings() {
+
+  const { userDetails } = useContext(UserContext);
+
+  const [listings, setListings] = useState([]);
+
+  useEffect((() => {
+    axios.get(`http://localhost:8001/listings?creatorId=${userDetails.id}`).then((result) => {
+      setListings(result.data);
+    })
+  }), [userDetails])
+
+  const [listing, setListing] = useState("")
+
+	const handleListingChange = function () {
+    setListing(this);
+	}
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2} columns={16}>
         <Grid item xs={8}>
           <Item>
-            <MyListingItem />
-            <MyListingItem />
-            <MyListingItem />
-            <MyListingItem />
+            {listings.map((listing) => {
+              return <MyListingItem onClick={handleListingChange.bind(listing)} listing={listing} />
+            })}
           </Item>
         </Grid>
         <Grid item xs={8}>
           <Item>
-            <ListingQuickView />
-            <OffersList />
+            <ListingQuickView listing={listing}/>
+            <OffersList listingId={listing.id} />
           </Item>
         </Grid>
       </Grid>
