a30881fdb905ada63db124d5e8ee34adadec51b9
Matt Parisien <matthewparisien4@gmail.com>
2022-02-08 14:44:26 -0500

    Complete conditional rendering of react router components

--- client/src/styles/globalStyles.js
+++ client/src/styles/globalStyles.js
@@ -1,14 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
+const GUTTER = "10vw";
+
 export const GlobalStyles = createGlobalStyle`
   html, body, .App, #root, main {
     height: 100%;
-    
   };
 
+
   main {
-    padding-top: 4.2rem;
-    padding-left: 10vw;
-    padding-right: 10vw;
-  };
+    .content-inner {
+      height: 100%;
+      padding-top: 4.2rem;
+    }
+  }
+
+ 
 `;
