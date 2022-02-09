64f049a233437715a8a3d027c5083b1c46e0d5ed
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-02-08 20:45:12 -0330

    creates new route for listing_categories post

--- server/src/routes/categories.js
+++ server/src/routes/categories.js
@@ -11,6 +11,17 @@ module.exports = db => {
     return router;
 };
 
+router.post("/categories/listings", (request, response) => {
+    const { categoryId, listingId } = request.body;
+
+    db.query(
+        `INSERT INTO listing_categories (category_id, listing_id) VALUES ($1::integer, $2::integer) RETURNING *;`,
+        [categoryId, listingId]
+    ).then((result) => {
+        response.status(201).json(result.rows[0]);
+    });
+});
+
 // API documentation for Open API below
 
 module.exports.apiDocs = {
@@ -53,5 +64,28 @@ module.exports.apiDocs = {
             }
         }
 
-    }
+    },
+    "categories/listings": {
+      
+            "post": {
+                "description": "Create a listing category relation",
+                "tags": ["categories"],
+                "requestBody": {
+                    "description": "listing and category ids",
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "type": "object",
+                            },
+                            "example": { "listingId": 2, "categoryId": 3 }
+                        }
+                    }
+                },
+                "responses": {
+                    201: {
+                        "description": "Listing category connection created",
+                    },
+                }
+            }
+        }
 }
