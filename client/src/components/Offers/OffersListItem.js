7eea0a95302e7c6687f5e29d0a86b81b898f2ead
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-02-09 08:44:41 -0800

    Added accept and decline buttons for offers

--- client/src/components/Offers/OffersListItem.js
+++ client/src/components/Offers/OffersListItem.js
@@ -1,9 +1,8 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
-import ButtonBase from "@mui/material/ButtonBase";
+import { Button, Alert } from "@mui/material";
 
 export default function OffersListItem(props) {
 //to do on this page - add rating count and hyperlink it to the ratings view
@@ -29,7 +28,32 @@ export default function OffersListItem(props) {
         </Grid>
         <Grid item>
           <Typography variant="subtitle1" component="div">
-            $500.00
+          <Button
+								size={"large"}
+								type='submit'
+								color='primary'
+								variant='contained'
+								sx={{ marginTop: 5 }}
+							>
+								Accept
+							</Button>
+              <Button
+								size={"large"}
+								type='submit'
+								color='primary'
+								variant='contained'
+								sx={{ marginTop: 5 }}
+							>
+								Decline
+							</Button>
+							{/* {errors &&
+								errors.map(err => {
+									return (
+										<Alert severity='error' sx={{ marginTop: ELEMENTSPACING }}>
+											{err.message}
+										</Alert>
+									);
+								})} */}
           </Typography>
         </Grid>
       </Grid>
