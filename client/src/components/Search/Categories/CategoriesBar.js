be0147cfd15d532c413fecb8708dcf04cec5a36a
Matt Parisien <matthewparisien4@gmail.com>
2022-02-10 08:42:44 -0500

    Implement successful category selection

--- client/src/components/Search/Categories/CategoriesBar.js
+++ client/src/components/Search/Categories/CategoriesBar.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography, Box } from "@mui/material";
 import { Chip, Paper } from "@mui/material";
 import Chips from "./Chips";
 
-function CategoriesBar({categories}) {
+function CategoriesBar({ categories }) {
 	const barStyle = {
-		
 		height: "100vh",
 		width: "400px",
 		p: 4,
 	};
+
+	const [selected, setSelected] = useState(null);
+
+	const handleSelected = (categoryId) => {
+		setSelected(categoryId)
+		console.log(categoryId)
+	}
 
 	return (
 		<Paper elevation={5}>
 			<Box sx={barStyle}>
 				<Typography>Recommended Topics</Typography>
 				<Box>
-					<Chips categories={categories}/>
+					<Chips
+						categories={categories}
+						selected={selected}
+						setSelected={handleSelected}
+					/>
 				</Box>
-        
 			</Box>
 		</Paper>
 	);
