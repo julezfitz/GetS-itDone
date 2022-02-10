b194c7d7c527a24c79e706f44e3c6b454ecdca4b
Matt Parisien <matthewparisien4@gmail.com>
2022-02-10 08:29:17 -0500

    Add categories successfully

--- client/src/components/Search/Categories/CategoriesBar.js
+++ client/src/components/Search/Categories/CategoriesBar.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Typography, Box } from "@mui/material";
+import { Chip, Paper } from "@mui/material";
+import Chips from "./Chips";
 
-function Categories() {
-  return (
-    <div>Categories</div>
-  )
+function CategoriesBar({categories}) {
+	const barStyle = {
+		
+		height: "100vh",
+		width: "400px",
+		p: 4,
+	};
+
+	return (
+		<Paper elevation={5}>
+			<Box sx={barStyle}>
+				<Typography>Recommended Topics</Typography>
+				<Box>
+					<Chips categories={categories}/>
+				</Box>
+        
+			</Box>
+		</Paper>
+	);
 }
 
-export default Categories
+export default CategoriesBar;
