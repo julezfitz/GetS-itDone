68758de2cd33d1078097a13d97f8acb58e292257
Matt Parisien <matthewparisien4@gmail.com>
2022-02-12 12:57:26 -0500

    Switch to dark mode

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,77 @@ import Application from "./components/Application";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider, useTheme, createTheme } from "@mui/material/styles";
-import { darkTheme } from "./styles/theme";
 
 //Our custom theme
+const darkTheme = createTheme({
+	palette: {
+		mode: "dark",
+		primary: {
+			main: "rgba(255,255,255,.05)",
+		},
+		secondary: {
+			main: "#ED6E23",
+			mainGradient: 'mainGradient: "linear-gradient(45deg, #ED6E23, #EDA224)',
+		},
+	},
+	components: {
+		MuiButton: {
+			variants: [
+				{
+					props: { color: "secondary", size: "small" },
+					style: {
+						textTransform: "none",
+						background: `linear-gradient(45deg, #ED6E23, #EDA224)`,
+						borderRadius: "60px",
+						color: "white",
+						padding: "0.4rem 2.3rem",
+					},
+				},
+				{
+					props: { variant: "naked" },
+					style: {
+						color: "black",
+						backgroundColor: "none",
+						padding: "0px",
+					},
+				},
+				{
+					props: { color: "primary", size: "small" },
+					style: {
+						color: "hsla(0,0%,100%,0.4)",
+						background: "rgba(255,255,255,.05)",
+						padding: "0.4rem 2.3rem",
+					},
+				},
+			],
+			styleOverrides: {
+				root: {
+					textTransform: "none",
+					display: "flex",
+					borderRadius: "60px",
+					boxShadow: "none",
+				},
+			},
+		},
+		MuiAppBar: {
+			variants: [
+				{
+					props: { color: "primary" },
+					style: {
+						textTransform: "none",
+						background: `transparent`,
+						boxShadow: "none",
+					},
+				},
+			],
+			styleOverrides: {
+				root: {
+					textTransform: "none",
+				},
+			},
+		},
+	},
+});
 
 ReactDOM.render(
 	<React.StrictMode>
