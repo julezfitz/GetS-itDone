a6d312947b7efa2d8e080e343af9eef1f86ba7ab
Matt Parisien <matthewparisien4@gmail.com>
2022-02-12 12:21:54 -0500

    Implement logged in nav

---
+++ client/src/components/Navigation/NavLeft/LoggedIn.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "../SearchBar";
+import Logo from "../Logo";
+
+function LoggedIn() {
+	return (
+		<>
+			<SearchBar />
+		</>
+	);
+}
+
+export default LoggedIn;
