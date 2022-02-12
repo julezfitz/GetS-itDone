71fb5691ae9f6e25eb64562eaeed18f1096f4d80
Matt Parisien <matthewparisien4@gmail.com>
2022-02-12 08:36:23 -0500

    Finish hero

--- client/src/components/LoggedOut/Landing/three/Scene.js
+++ client/src/components/LoggedOut/Landing/three/Scene.js
@@ -4,7 +4,12 @@ import Model from "./Model";
 
 function Scene() {
 	return (
-		<div>hi</div>
+		<iframe
+			src='https://my.spline.design/untitled-32b0ae3738f4da296b43b5199b80aa4f/'
+			frameborder='0'
+			width='100%'
+			height='100%'
+		></iframe>
 		// <Suspense fallback={null}>
 		// 	<Canvas camera={{ position: [0, 0, 0] }}>
 		// 		<Model />
