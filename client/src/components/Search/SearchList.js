59cc90188c828501aa6c288f628ea39ca318362b
Matt Parisien <matthewparisien4@gmail.com>
2022-02-14 15:04:12 -0500

    Prettier

--- client/src/components/Search/SearchList.js
+++ client/src/components/Search/SearchList.js
@@ -9,8 +9,6 @@ import { Grid, Item } from "@mui/material";
 import { Typography } from "@mui/material";
 import useTheme from "@mui/material/styles/useTheme";
 
-
-
 // const Item = styled(Paper)(({ theme }) => ({
 // 	...theme.typography.body2,
 // 	padding: theme.spacing(1),
@@ -21,7 +19,6 @@ import useTheme from "@mui/material/styles/useTheme";
 //If no chip is selected we can fetch all listings
 
 export default function SearchList({ keywords, listings }) {
-
 	const theme = useTheme();
 
 	const [listing, setListing] = useState({});
@@ -40,37 +37,44 @@ export default function SearchList({ keywords, listings }) {
 						transition: "300ms ease",
 						"&:hover > .MuiGrid-item": {
 							opacity: 0.5,
-							
 						},
 
 						".MuiGrid-item:hover": {
 							opacity: 1,
 						},
 						".MuiGrid-item:hover .MuiTypography-root": {
-							color: "white !important"
+							color: "white !important",
 						},
 					}}
 				>
-					{listings[0] && listings.map((listing, i) => {
-						return (
-							<Grid
-								item
-								xs={12}
-								md={12}
-								lg={6}
-								key={Math.random().toString(36).substr(2, 9)}
-							>
-							<SearchListItem
-								key={Math.random().toString(36).substr(2, 9)}
-								onChoice={handleListingChange}
-								listing={listing}
-							/>
-							</Grid>
-						);
-					})}
+					{listings[0] &&
+						listings.map((listing, i) => {
+							return (
+								<Grid
+									item
+									xs={12}
+									md={12}
+									lg={6}
+									key={Math.random().toString(36).substr(2, 9)}
+								>
+									<SearchListItem
+										key={Math.random().toString(36).substr(2, 9)}
+										onChoice={handleListingChange}
+										listing={listing}
+									/>
+								</Grid>
+							);
+						})}
 				</Grid>
 			</ListingDetails>
-			{!listings[0] && <Typography variant="h5" sx={{mt: 4, color: theme.palette.primary.grey}}>The are no current search results</Typography>}
+			{!listings[0] && (
+				<Typography
+					variant='h5'
+					sx={{ mt: 4, color: theme.palette.primary.grey }}
+				>
+					The are no current search results
+				</Typography>
+			)}
 		</Box>
 	);
 }
