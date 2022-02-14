eff0f392a34ac6f793298b5ac301d460e399e813
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-02-14 14:33:54 -0800

    Tweaked the app logo size and colouring

--- client/src/components/Navigation/Logo.js
+++ client/src/components/Navigation/Logo.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Box, Tooltip, IconButton, Avatar } from "@mui/material";
 import { Link } from "react-router-dom";
-import logo from "../../logo-white.png";
+import logo from "../../logo-black.png";
 
 function Logo() {
   return (
     <>
-      <Box sx={{ paddingRight:"5px", maxWidth: "150px" }}>
-        <Avatar src={logo} alt="Logo" />
+      <Box sx={{ paddingRight:"10px", maxWidth: "150px" }}>
+        <Avatar src={logo} alt="Logo" sx={{ width: 70, height: 70 }}/>
       </Box>
       <Box sx={{ maxWidth: "250px" }}>
         <Link to={"/"} style={{ marginRight: "3rem", color: "white" }}>
