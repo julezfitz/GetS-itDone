6e6f5861a2597d471f5d877b03c6bab6c904f5d1
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-02-14 14:19:20 -0800

    Restyling the layout of the create listing modal

--- client/src/components/Listings/New.js
+++ client/src/components/Listings/New.js
@@ -7,152 +7,153 @@ import { Button } from "@mui/material";
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { UserContext } from "../Application.js";
+import { FormGroup, FormControl } from "@mui/material";
 
 const style = {
-	position: "absolute",
-	top: "50%",
-	left: "50%",
-	transform: "translate(-50%, -50%)",
-	width: 400,
-	bgcolor: "background.paper",
-	border: "2px solid #000",
-	boxShadow: 24,
-	p: 10,
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 450,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  borderRadius: "10px",
+  boxShadow: 24,
+  p: 10,
+};
+
+const formGroupStyle = {
+  "& .MuiTextField-root": { flexGrow: 1 },
 };
 
 export default function NewListingModal({ open, handleClose }) {
-	const { userDetails } = useContext(UserContext);
+  const { userDetails } = useContext(UserContext);
 
-	const [newListing, setNewListing] = useState({});
+  const [newListing, setNewListing] = useState({});
 
-	let newListingDetails;
+  let newListingDetails;
 
-	const [category, setCategory] = useState("");
+  const [category, setCategory] = useState("");
 
-	const handleCategoryChange = category => {
-		setCategory(category);
-	};
+  const handleCategoryChange = (category) => {
+    setCategory(category);
+  };
 
-	const handleSubmit = e => {
-		e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-		let price = parseInt(e.target.elements.price.value);
+    let price = parseInt(e.target.elements.price.value);
 
-		//add image urls to this once they are added to the form
-		newListingDetails = {
-			creatorId: userDetails.id,
-			title: e.target.elements.title.value,
-			description: e.target.elements.description.value,
-			price: price,
-			city: e.target.elements.city.value,
-			province: userDetails.province,
-			country: userDetails.country,
-			postalCode: e.target.elements.postalCode.value,
-			image_1: e.target.elements.image_1.value,
-			image_2: e.target.elements.image_2.value,
-			image_3: e.target.elements.image_3.value,
-		};
+    //add image urls to this once they are added to the form
+    newListingDetails = {
+      creatorId: userDetails.id,
+      title: e.target.elements.title.value,
+      description: e.target.elements.description.value,
+      price: price,
+      city: e.target.elements.city.value,
+      province: userDetails.province,
+      country: userDetails.country,
+      postalCode: e.target.elements.postalCode.value,
+      image_1: e.target.elements.image_1.value,
+      image_2: e.target.elements.image_2.value,
+      image_3: e.target.elements.image_3.value,
+    };
 
-		axios
-			.post(`http://localhost:8001/listings`, newListingDetails)
-			.then(result => {
-				let categoryForListing = {
-					categoryId: category,
-					listingId: result.data.id,
-				};
-				return axios.post(
-					`http://localhost:8001/categories/listings`,
-					categoryForListing
-				);
-			})
-			.then(result => {
-				console.log(result.data);
-			});
-	};
+    axios
+      .post(`http://localhost:8001/listings`, newListingDetails)
+      .then((result) => {
+        let categoryForListing = {
+          categoryId: category,
+          listingId: result.data.id,
+        };
+        return axios.post(
+          `http://localhost:8001/categories/listings`,
+          categoryForListing
+        );
+      })
+      .then((result) => {
+        console.log(result.data);
+      });
+  };
 
-	return (
-		<div>
-			<Modal
-				open={open}
-				onClose={handleClose}
-				aria-labelledby='modal-modal-title'
-				aria-describedby='modal-modal-description'
-			>
-				<Box sx={style}>
-					<Typography id='modal-modal-title' variant='h6' component='h2'>
-						Create New Listing
-					</Typography>
-					{/* <Typography id='modal-modal-description' sx={{ mt: 2 }}> */}
-					<Box
-						component='form'
-						onSubmit={handleSubmit}
-						sx={{ "& .MuiTextField-root": { m: 1, width: "25ch" } }}
-						noValidate
-						autoComplete='off'
-					>
-						<div>
-							<TextField
-								required
-								id='outlined-required'
-								name='title'
-								label='Title'
-							/>
-							<CategoryList onSelect={handleCategoryChange} />
-							<TextField
-								required
-								id='1c'
-								label='Description'
-								name='description'
-							/>
-							<TextField
-								required
-								id='2c'
-								name='city'
-								label='City'
-							/>
-							<TextField
-								required
-								id='3c'
-								label='Postal Code'
-								name='postalCode'
-							/>
-							<TextField
-								required
-								id='4c'
-								label='Price'
-								name='price'
-							/>
-							<TextField
-								id='5c'
-								name='image_1'
-								label='Image URL'
-							/>
-							<TextField
-								id='6c'
-								name='image_2'
-								label='Image URL 2'
-							/>
-							<TextField
-								id='7c'
-								name='image_3'
-								label='Image URL 3'
-							/>
-						</div>
-						<Button
-							size='large'
-							type='submit'
-							value='Submit'
-							variant='contained'
-							color="secondary"
-							fullWidth
-							sx={{mt: 4}}
-						>
-							Create
-						</Button>
-					</Box>
-					{/* </Typography> */}
-				</Box>
-			</Modal>
-		</div>
-	);
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            sx={{ mb: 5, textAlign: "center", fontFamily: "Inter" }}
+          >
+            Create New Listing
+          </Typography>
+
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{ "& .MuiTextField-root": { m: 1 }, position: "relative" }}
+            noValidate
+            autoComplete="off"
+          >
+            <FormGroup row sx={formGroupStyle}>
+              <TextField
+                required
+                id="outlined-required"
+                name="title"
+                label="Title"
+              />
+            </FormGroup>
+            <FormGroup row sx={formGroupStyle}>
+              <CategoryList onSelect={handleCategoryChange} />
+            </FormGroup>
+            <FormGroup row sx={formGroupStyle}>
+              <TextField
+                required
+                id="1c"
+                label="Description"
+                name="description"
+              />
+            </FormGroup>
+            <FormGroup row sx={formGroupStyle}>
+              <TextField required id="2c" name="city" label="City" />
+              <TextField
+                required
+                id="3c"
+                label="Postal Code"
+                name="postalCode"
+              />
+            </FormGroup>
+            <FormGroup row sx={formGroupStyle}>
+              <TextField required id="4c" label="Price" name="price" />
+            </FormGroup>
+            <FormGroup row sx={formGroupStyle}>
+              <TextField id="5c" name="image_1" label="Image URL" />
+            </FormGroup>
+            <FormGroup row sx={formGroupStyle}>
+              <TextField id="6c" name="image_2" label="Image URL 2" />
+              <TextField id="7c" name="image_3" label="Image URL 3" />
+            </FormGroup>
+
+            <Button
+              size="large"
+              type="submit"
+              value="Submit"
+              variant="contained"
+              color="secondary"
+              fullWidth
+              sx={{ mt: 5 }}
+            >
+              Create Listing
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
 }
