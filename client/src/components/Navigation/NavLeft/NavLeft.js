0e703970321ea5da2d1479d59984cecc275ed616
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-02-14 11:40:51 -0800

    Removed add rating. Changed format of create new listing

--- client/src/components/Navigation/NavLeft/NavLeft.js
+++ client/src/components/Navigation/NavLeft/NavLeft.js
@@ -1,9 +1,5 @@
 import React from "react";
-import { Box, Button } from "@mui/material";
-import RegisterModal from "../../User/Registration/Register";
-import LoginModal from "../../User/Login";
-import { Link } from "react-router-dom";
-import { Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
 import Logo from "../Logo";
 import LoggedOut from "./LoggedOut";
 import LoggedIn from "./LoggedIn";
