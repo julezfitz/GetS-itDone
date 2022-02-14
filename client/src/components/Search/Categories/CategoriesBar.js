3c644608f12b360ee0dc9c1d8d2be9721f4682bf
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-02-14 19:25:07 -0330

    adds in search menus

--- client/src/components/Search/Categories/CategoriesBar.js
+++ client/src/components/Search/Categories/CategoriesBar.js
@@ -4,6 +4,11 @@ import { Chip, Paper } from "@mui/material";
 import Chips from "./Chips";
 import { Divider } from "@mui/material";
 import { Container } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Grid from "@mui/material/Grid";
 
 function CategoriesBar({
 	categories,
@@ -25,18 +30,62 @@ function CategoriesBar({
 	return (
 		<>
 			<Box style={barStyle}>
-				<Box style={{ width: "100%" }}>
-					<Typography sx={{ color: "white" }}>Filter by Category</Typography>
-					<Box>
-						<Chips
-							categories={categories}
-							selected={selectedChip}
-							setSelected={handleSelectedChip}
-							handleClearSelection={handleClearSelection}
-							emptySearch={emptySearch}
-						/>
-					</Box>
-				</Box>
+				<Grid container direction='column' spacing={3}>
+
+					<Grid item style={{ display: "flex", gap: "1rem" }}>
+						<Box style={{ width: "100%" }}>
+						<Typography sx={{ color: "white" }}>Sort by</Typography>
+							<FormControl size='string' style={{ width: "55%" }}>
+								<InputLabel id="sortby-label">Sort by</InputLabel>
+								<Select
+									labelId="sortby-label"
+									id="demo-simple-select"
+									label="Search Filters"
+								// onChange={handleSortChange}
+								>
+									<MenuItem key={Math.random().toString(36).substr(2, 9)}>
+										Price
+									</MenuItem>
+									<MenuItem key={Math.random().toString(36).substr(2, 9)}>
+										Date Listed
+									</MenuItem>
+								</Select>
+								</FormControl>
+								<FormControl size='string' style={{ width: "35%", marginLeft:'1em' }}>
+								<InputLabel id="orderby-label">Order by</InputLabel>
+								<Select
+									labelId="orderby-label"
+									id="demo-simple-select"
+									label="Search Filters"
+								// onChange={handleOrderChange}
+								>
+									<MenuItem key={Math.random().toString(36).substr(2, 9)}>
+										Ascending
+									</MenuItem>
+									<MenuItem key={Math.random().toString(36).substr(2, 9)}>
+										Descending
+									</MenuItem>
+								</Select>
+							</FormControl>
+						</Box>
+					</Grid>
+
+					<Grid item>
+						<Box style={{ width: "100%" }}>
+							<Typography sx={{ color: "white" }}>Filter by Category</Typography>
+							<Box>
+								<Chips
+									categories={categories}
+									selected={selectedChip}
+									setSelected={handleSelectedChip}
+									handleClearSelection={handleClearSelection}
+									emptySearch={emptySearch}
+								/>
+							</Box>
+						</Box>
+					</Grid>
+
+				</Grid>
 			</Box>
 		</>
 	);
