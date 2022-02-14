22ced0dcba19cb657c39a19c9e1458b5cb1f145c
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-02-14 13:13:14 -0800

    Added a logo for the app

--- client/src/components/Navigation/Logo.js
+++ client/src/components/Navigation/Logo.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Box, Tooltip, IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
+import logo from "../../logo.png";
 
 function Logo() {
-	return (
-		<Link to={"/"} style={{ marginRight: "3rem", color: "white" }}>
-			Get S*it Done
-		</Link>
-	);
+  return (
+    //   <Box sx={{ maxWidth: "150px" }}>
+    //     <Tooltip>
+    //       <IconButton sx={{ p: 0, color: "white" }}>
+    //        <img src={logo} alt="Logo" />
+    //       </IconButton>
+    //     </Tooltip>
+    //   </Box>
+      <Link to={"/"} style={{ marginRight: "3rem", color: "white" }}>
+        Get S*it Done
+      </Link>
+  );
 }
 
 export default Logo;
