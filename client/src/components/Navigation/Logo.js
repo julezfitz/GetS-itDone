09117e19bc44aec076c6acbca4f7e3c552fa5163
BMWSawyer <bradley.mw.sawyer@gmail.com>
2022-02-14 13:48:51 -0800

    Fixed the logo in the Navbar

--- client/src/components/Navigation/Logo.js
+++ client/src/components/Navigation/Logo.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { Box, Tooltip, IconButton } from "@mui/material";
+import { Box, Tooltip, IconButton, Avatar } from "@mui/material";
 import { Link } from "react-router-dom";
 import logo from "../../logo.png";
 
 function Logo() {
   return (
-    //   <Box sx={{ maxWidth: "150px" }}>
-    //     <Tooltip>
-    //       <IconButton sx={{ p: 0, color: "white" }}>
-    //        <img src={logo} alt="Logo" />
-    //       </IconButton>
-    //     </Tooltip>
-    //   </Box>
-      <Link to={"/"} style={{ marginRight: "3rem", color: "white" }}>
-        Get S*it Done
-      </Link>
+    <>
+      <Box sx={{ paddingRight:"5px", maxWidth: "150px" }}>
+        <Avatar src={logo} alt="Logo" />
+      </Box>
+      <Box sx={{ maxWidth: "250px" }}>
+        <Link to={"/"} style={{ marginRight: "3rem", color: "white" }}>
+          Get S*it Done
+        </Link>
+      </Box>
+    </>
   );
 }
 
