987a9f2513bb63b6b60cab8e1f593bc5f7a50aa7
Matt Parisien <matthewparisien4@gmail.com>
2022-02-15 08:12:23 -0500

    Move theme into seperate global styles folder

--- client/src/styles/globalStyles.js
+++ client/src/styles/globalStyles.js
@@ -1,23 +1,217 @@
 import { createGlobalStyle } from "styled-components";
 import Kobe from "../assets/fonts/Kobe.woff";
-import { DEEPBLACK, FOOTERHEIGHT } from "..";
+import { createTheme } from "@mui/material/styles";
 
 const GUTTER = "10vw";
 
+//Color variables
+export const DEEPBLACK = "black";
+export const OFFBLACK = "#1F1F1F";
+export const LIGHT = "#FFFFFF";
+export const GREY = "#999999";
+export const GRADIENT = "linear-gradient(45deg, #ED6E23, #EDA224)";
+export const ORANGE = "#ED6E23";
+
+//Constants
+export const FOOTERHEIGHT = "80vh";
+export const TRANSITION = "300ms ease";
+
+//Our custom theme
+export const darkTheme = createTheme({
+	transitions: {
+		easing: {
+			// This is the most common easing curve.
+			easeInOut: "cubic-bezier(0.4, 0, 0.2, 1)",
+			// Objects enter the screen at full velocity from off-screen and
+			// slowly decelerate to a resting point.
+			easeOut: "cubic-bezier(0.0, 0, 0.2, 1)",
+			// Objects leave the screen at full velocity. They do not decelerate when off-screen.
+			easeIn: "cubic-bezier(0.4, 0, 1, 1)",
+			// The sharp curve is used by objects that may return to the screen at any time.
+			sharp: "cubic-bezier(0.4, 0, 0.6, 1)",
+		},
+		duration: {
+			shortest: 150,
+			shorter: 200,
+			short: 250,
+			// most basic recommended timing
+			standard: 300,
+			// this is to be used in complex animations
+			complex: 375,
+			// recommended when something is entering screen
+			enteringScreen: 225,
+			// recommended when something is leaving screen
+			leavingScreen: 195,
+		},
+	},
+	typography: {
+		fontSize: 13,
+		fontWeight: 200,
+		fontFamily: [
+			"-apple-system",
+			"BlinkMacSystemFont",
+			'"Segoe UI"',
+			"Roboto",
+			'"Helvetica Neue"',
+			"Arial",
+			"sans-serif",
+			'"Apple Color Emoji"',
+			'"Segoe UI Emoji"',
+			'"Segoe UI Symbol"',
+		].join(","),
+		subtitle1: {
+			color: GREY,
+		},
+		h2: {
+			color: LIGHT,
+		},
+		body2: {
+			color: `${GREY} !important`,
+		},
+	},
+	palette: {
+		text: {
+			primary: LIGHT,
+			secondary: GREY,
+		},
+		mode: "dark",
+		primary: {
+			main: OFFBLACK,
+			grey: GREY,
+		},
+		secondary: {
+			main: "#ED6E23",
+			mainGradient: GRADIENT,
+		},
+	},
+	overrides: {
+		MuiOutlinedInput: {
+			input: {
+				"&:-webkit-autofill": {
+					"-webkit-box-shadow": "none",
+					"-webkit-text-fill-color": "#fff",
+				},
+			},
+			fieldset: {
+				display: "none",
+			},
+		},
+	},
+
+	components: {
+		MuiBadge: {
+			styleOverrides: {
+				root: {
+					span: {
+						backgroundColor: ORANGE,
+					},
+				},
+			},
+		},
+		MuiOutlinedInput: {
+			styleOverrides: {
+				root: {
+					backgroundColor: OFFBLACK,
+					backgroundImage: "none",
+				},
+			},
+		},
+		MuiGrid: {
+			styleOverrides: {
+				root: {
+					transition: TRANSITION,
+				},
+			},
+		},
+		MuiPaper: {
+			styleOverrides: {
+				root: {
+					backgroundColor: OFFBLACK,
+					backgroundImage: "none",
+				},
+			},
+		},
+
+		MuiAlert: {
+			styleOverrides: {
+				root: {
+					background: "transparent",
+				},
+			},
+		},
+		MuiButton: {
+			variants: [
+				{
+					props: { color: "secondary", size: "small" },
+					style: {
+						textTransform: "none",
+						background: GRADIENT,
+						borderRadius: "60px",
+						color: "white",
+						padding: "0.4rem 2.3rem",
+					},
+				},
+				{
+					props: { variant: "naked" },
+					style: {
+						color: "white",
+						backgroundColor: "none",
+						padding: "0px",
+					},
+				},
+				{
+					props: { color: "primary", size: "small" },
+					style: {
+						color: "hsla(0,0%,100%,0.4)",
+						background: "rgba(255,255,255,.05)",
+						padding: "0.4rem 2.3rem",
+					},
+				},
+				{
+					props: { color: "secondary", size: "large" },
+					style: {
+						color: LIGHT,
+						background: GRADIENT,
+						padding: "0.4rem 2.3rem",
+					},
+				},
+			],
+			styleOverrides: {
+				root: {
+					textTransform: "none",
+					display: "flex",
+					borderRadius: "60px",
+					boxShadow: "none",
+				},
+			},
+		},
+		MuiAppBar: {
+			variants: [
+				{
+					props: { color: "primary" },
+					style: {
+						textTransform: "none",
+						background: `transparent`,
+						boxShadow: "none",
+					},
+				},
+			],
+			styleOverrides: {
+				root: {
+					textTransform: "none",
+				},
+			},
+		},
+	},
+});
+
 export const GlobalStyles = createGlobalStyle`
 
-h1, h2, h3, h4, h5 {
-  color: white;
-}
-  
 @font-face {
   font-family: 'Kobe',
   src: url(${Kobe})format('woff');
 }
 
-
-
-  
   .accent {
     color: #ED6E23;
   }
@@ -32,14 +226,11 @@ h1, h2, h3, h4, h5 {
     background-color: #111;
   };
 
- 
 
   h1, h2, h3, h4, h5  {
     font-family: Inter;
+    color: white;
   }
-
-  
-
 
   a {
     text-decoration: none;
@@ -60,8 +251,4 @@ h1, h2, h3, h4, h5 {
     padding-bottom: 10rem;
     box-shadow: -3px -5px 48px 4px rgba(0,0,0,.5);
   }
-
-
-
- 
 `;
