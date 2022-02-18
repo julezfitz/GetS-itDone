30697b2bc12a798fae583baa02d7fce1e75ba6b3
Julie Fitzpatrick <juliefitzpatrick@Julies-MacBook-Air.local>
2022-02-18 09:42:13 -0330

    fixes clear search button

--- client/src/components/LoggedOut/Landing/Spline/Scene.js
+++ client/src/components/LoggedOut/Landing/Spline/Scene.js
@@ -4,7 +4,7 @@ function Scene() {
 	return (
 		<iframe
 			src='https://my.spline.design/primitivescopy-281f583b9516daa86ae31e7a6b1128ab/'
-			frameborder='0'
+			frameBorder='0'
 			width='100%'
 			height='100%'
 			scrolling="no"
